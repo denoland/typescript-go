bebece48079352eb87bc8e599d61d05481bce461
Nathan Whitaker <nathan@deno.com>
2025-12-01 13:45:30 -0800

    fmt

--- internal/lsp/server.go
+++ internal/lsp/server.go
@@ -996,7 +996,7 @@ func (s *Server) handleInitialized(ctx context.Context, params *lsproto.Initiali
 			DebounceDelay:          500 * time.Millisecond,
 			PushDiagnosticsEnabled: !disablePushDiagnostics,
 			Locale:                 s.locale,
-			MakeHost:           project.NewProjectHost,
+			MakeHost:               project.NewProjectHost,
 		},
 		FS:          s.fs,
 		Logger:      s.logger,
